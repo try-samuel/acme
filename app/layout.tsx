2158764ab588be372766bbded8773df025b7f1ae
weird-samuel <fiyinfoluwasamuel580@gmail.com>
2024-05-24 17:46:32 +0100

    added auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,15 @@
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+import { Metadata } from 'next';
 
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme',
+    default: 'Acme',
+  },
+  // description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://acme-gamma-five.vercel.app/'),
+};
 export default function RootLayout({
   children,
 }: {
